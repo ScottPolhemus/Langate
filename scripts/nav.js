26e3d164d53e6c0eca7952bd6725b688db8853c5
LucasJBallasy <lucas.ballasy@Administrators-MacBook-Pro.local>
2016-10-30 22:42:01 -0400

    more responsive nav fixes

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){ 
 	$(".menu-burger").click(function(){
   		$(".menu-burger").toggleClass("open");
+  		$("main").toggleClass("slide");
+  		$(".nav-links").toggleClass("open");
 	});
 })
