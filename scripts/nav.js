01e0d76ce0c01f8f96fcc3aa3f3fca0b07b20aee
Scott Polhemus <scott@polhem.us>
2016-11-02 21:12:45 -0400

    Merge frontend updates

---
+++ scripts/nav.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".menu-burger").click(function(){
+    $(".menu-burger").toggleClass("open");
+    $("main").toggleClass("slide");
+    $(".nav-links").toggleClass("open");
+  });
+})
