e27c42e137243a4cf2e80e4e8948af732bbee2db
LucasJBallasy <lucas.ballasy@Administrators-MacBook-Pro.local>
2016-11-05 20:15:43 -0400

    more footer, new mobile menu, accordion progress

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,4 +4,33 @@ $(document).ready(function(){
   		$("main").toggleClass("slide");
   		$(".nav-wrapper").toggleClass("open");
 	});
+
+	$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+});
+
+
 })
