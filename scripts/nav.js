f13e9c243c98213adafd45e83adb2f800b59bb9b
Scott Polhemus <scott@polhem.us>
2016-11-17 11:39:40 -0500

    Fe updates & work template

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,63 @@
 $(document).ready(function(){
-  $(".menu-burger").click(function(){
-    $(".menu-burger").toggleClass("open");
-    $("main").toggleClass("slide");
-    $(".nav-links").toggleClass("open");
-  });
+
+	//MOBILE BURGER MENU
+
+	$(".menu-burger").click(function(){
+  		$(".menu-burger").toggleClass("open");
+  		$("main").toggleClass("slide");
+  		$(".mobile-nav").toggleClass("open");
+	});
+
+	//ACCORDION MOBILE
+
+	$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}
+
+		var accordion = new Accordion($('#accordion'), false);
+	});
+
+	//QUOTE SLIDESHOW
+
+	jQuery(function($) {
+
+	  var slideInterval = 3000,
+	    fadeDuration = 600,
+	    $slideshow = $('.quote-container'),
+	    $slides = $slideshow.children();
+
+	  $slides.eq(0).fadeIn(fadeDuration);
+
+	  setInterval(function() {
+	    $slides = $slideshow.children();
+	    $slides.eq(0).fadeOut(fadeDuration,
+
+	  function() {
+	      $(this).appendTo($slideshow);
+	    });
+	    $slides.eq(1).fadeIn(fadeDuration);
+	  }, slideInterval);
+
+	});
+
 })
